a251f67fb8a1b72a105ec898dee3762268cdb00f
Valentin <itskuits@yandex.ru>
2020-09-08 07:59:49 +0300

    Start-to-do

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import List from "./components/List/List";
+
+function App() {
+  return (
+    <div>
+      <List/>
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/components/List/List.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+class List extends React.Component {
+
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default List;
