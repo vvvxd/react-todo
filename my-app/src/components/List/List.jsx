021aa9be61875165b2f4ec9d9bc7f4ae6784dab1
Valentin <itskuits@yandex.ru>
2020-09-08 08:10:59 +0300

    Statick app

--- my-app/src/components/List/List.jsx
+++ my-app/src/components/List/List.jsx
@@ -1,12 +1,17 @@
 import React from "react";
+import './List.css';
+import ShowList from "./ShowList/ShowList";
+import Editor from "./Editor/Editor";
 
 class List extends React.Component {
-
-
     render() {
         return (
             <div>
-
+                <div id="list" className="todo">
+                    <h2 className="todo__caption">Список дел</h2>
+                    <ShowList/>
+                    <Editor/>
+                </div>
             </div>
         )
     }
