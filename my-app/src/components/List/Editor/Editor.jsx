9ec6ab324c9eca76a9ed18a8d84cf94ff7df5493
Valentin <itskuits@yandex.ru>
2020-09-09 08:18:05 +0300

    Add message to state

--- my-app/src/components/List/Editor/Editor.jsx
+++ my-app/src/components/List/Editor/Editor.jsx
@@ -1,12 +1,20 @@
 import React from "react";
 
-const Editor = (props)=>{
-return(
-    <div className="todo__controls">
-        <input type="text" className="todo__text"/>
-        <button disabled="disabled" className="todo__add">Добавить</button>
-    </div>
-)
+const Editor = (props) => {
+    const changeInput = (e) => {
+        let text = e.target.value;
+        props.changeInput(text);
+    };
+    const addNewItem = () => {
+        props.addNewItem();
+    };
+
+    return (
+        <div className="todo__controls">
+            <input value={props.nowText} type="text" className="todo__text" onChange={changeInput}/>
+            <button disabled={props.disabled} className="todo__add" onClick={addNewItem} >Добавить</button>
+        </div>
+    )
 };
 export default Editor;
 
--- my-app/src/components/List/List.jsx
+++ my-app/src/components/List/List.jsx
@@ -4,13 +4,42 @@ import ShowList from "./ShowList/ShowList";
 import Editor from "./Editor/Editor";
 
 class List extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            nowText: '',
+            itemsListArr: [],
+            disabled: true,
+        };
+
+        this.addNewItem = this.addNewItem.bind(this);
+        this.changeInput = this.changeInput.bind(this);
+    }
+
+    addNewItem() {
+        let newArray = [...this.state.itemsListArr];
+        newArray.push(this.state.nowText);
+        this.setState({
+            itemsListArr: newArray,
+            disabled:true,
+            nowText: '',
+        });
+    }
+
+    changeInput(value) {
+        this.setState({
+            nowText: value,
+            disabled:false,
+        });
+    }
+
     render() {
         return (
             <div>
                 <div id="list" className="todo">
                     <h2 className="todo__caption">Список дел</h2>
                     <ShowList/>
-                    <Editor/>
+                    <Editor disabled={this.state.disabled} nowText={this.state.nowText} changeInput={this.changeInput} addNewItem={this.addNewItem}/>
                 </div>
             </div>
         )
